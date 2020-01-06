3823ea75140bb9b619b52c3315f97956279049eb
jamil <jrasulch@gmail.com>
2020-01-05 23:07:51 -0800

    Committing Java project changes

--- JavaTestProject/src/GitTest.java
+++ JavaTestProject/src/GitTest.java
@@ -4,7 +4,12 @@ public class GitTest {
 	public static void main(String[] args) {
 
 		System.out.print("Print 1 to 10 numbers : ");
-		for (int i = 0; i < 10; i++) {
+		for (int i = 1; i < 11; i++) {
+			System.out.print(i + " ");
+		}
+
+		System.out.print("\nPrint 10 to 1 numbers : ");
+		for (int i = 10; i > 0; i--) {
 			System.out.print(i + " ");
 		}
 
