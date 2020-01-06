5951b96b16e2801a87e632d3c18eb21d1b8877e0
jamil <jrasulch@gmail.com>
2020-01-05 22:39:30 -0800

    Adding Java Project

---
+++ JavaTestProject/src/GitTest.java
@@ -0,0 +1,12 @@
+
+// Print 1 to 10 numbers
+public class GitTest {
+	public static void main(String[] args) {
+
+		System.out.print("Print 1 to 10 numbers : ");
+		for (int i = 0; i < 10; i++) {
+			System.out.print(i + " ");
+		}
+
+	}
+}
